b22581edf77dba87286054217c28640d9cd9f840
Jennifer <buenojenniferx3@gmail.com>
2024-04-22 15:30:37 -0400

    Your commit message here

---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,26 @@
+package com.ps;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       String[] quotes = {"You have to believe in yourself when no one else does.", "The only way to do great work is to love what you do.",
+               "Believe you can and you're halfway there.",
+               "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+               "The future belongs to those who believe in the beauty of their dreams.",
+               "In the midst of winter, I found there was, within me, an invincible summer.",
+               "The only limit to our realization of tomorrow will be our doubts of today.",
+               "Do not wait; the time will never be 'just right.' Start where you stand, and work with whatever tools you may have at your command, and better tools will be found as you go along.",
+               "You are never too old to set another goal or to dream a new dream.",
+               "The only way to achieve the impossible is to believe it is possible.",
+               "You miss 100% of the shots you don't take."
+       };
+
+           Scanner scanner = new Scanner(System.in);
+            System.out.println("Please select a number between 1 to 10");
+            int quoteNumber = scanner.nextInt();
+
+       }
+
+
+    }
